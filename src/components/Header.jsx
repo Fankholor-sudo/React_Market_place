64d00dcdb1c9999b6fc6020a0afc695fc9f0e01a
Charles Makgakga <60372959+XavierCharlie@users.noreply.github.com>
2021-04-07 18:10:21 +0200

    Landing Page Header Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import DropdownBtn from "./dropdownBtn";
+import Example from "./regNavBar";
+import Search from "./search";
+import {Favorites, Cart} from "./favCart";
+
+function Header(){
+    return(
+        // <header>
+            <div className="parent">
+                <img className="logo" src="./images/LOGO.svg" alt = "App Logo"/>
+                
+                <div className="dropSearch">
+                    <DropdownBtn />
+                    <Search />
+                </div>
+                
+                <div className="regFavCart">
+                    <Example/>
+
+                    <div className="favCart">
+                        <Favorites />
+                        <Cart />
+                    </div>
+                </div>
+                
+            </div>
+            
+        // </header>
+    );
+}
+
+export default Header;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import CatNavBar from "./catNavBar";
+
+
+function App() {
+  return(
+  <div className="">
+    <Header />
+    <CatNavBar />
+  </div>);
+}
+
+export default App;
