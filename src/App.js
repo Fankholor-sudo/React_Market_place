44ccc9f11ce2fd589de330653aaa1e0344ef6b15
2185695 <2185695@students.wits.ac.za>
2021-04-11 03:02:31 +0200

    login-register forms.need back end for complition

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Alert, Row, Col, Image } from 'react-bootstrap';
-import Register from './components/Register';
-import RegImgHolder from './components/RegImgHolder';
-import Login from './components/Login';
-import LgnImgHolder from './components/LgnImgHolder';
+import Register from './components/LoginReg/Register';
+import LoginForm from './components/LoginReg/LoginForm';
+import { withRouter } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Row class="d-flex">
-          <div class="d-flex align-items-center" >
-            <Image src='./img/trans_logo.png' style={{marginLeft:'50px' ,marginTop:'5px',width: '50px'}}/>
-          </div>
-          <p style={{ marginLeft: '3%',marginTop: '30px' ,fontSize:'16px'}}>
-            We have everything that you need and like. Clothing | Technology | Books | Accessories | Sports | Stationary</p>
-          <p style={{ marginLeft: '15%', marginTop: '30px' }}>
-            <a href='./'>Already have an account? Login</a></p>
-          {/* <p style={{marginLeft: '60%', marginTop:'110px'}}><a href='./'>Do not have an account? Register</a></p> */}
-        </Row>
-      </header>
+      <Router>
+        <Switch>
+        <Route path="/" exact={true}>
+              <LoginForm />
+        </Route>
 
-      <Row className="landing">
-        <Col><Register /></Col>
-        <Col><RegImgHolder /></Col>
-      </Row>
-       {/* <Row className="landing">
-        <Col><Login /></Col>
-        <Col><LgnImgHolder /></Col>
-      </Row> */}
+        <Route path="/LoginForm" exact={true}>
+              <LoginForm />
+        </Route>
+
+        <Route path="/Register" exact={true}>
+              <Register />
+        </Route>
+        
+        </Switch>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
