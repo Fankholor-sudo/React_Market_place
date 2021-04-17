a37a951d642b3da3225ab4b69fb9afd259416a3b
Legend-45 <62772811+Legend-45@users.noreply.github.com>
2021-04-17 05:27:48 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<BrowserRouter>
+    <App />
+  </BrowserRouter>);
   const linkElement = screen.getByText('Password');
   expect(linkElement).toBeInTheDocument();
 });
