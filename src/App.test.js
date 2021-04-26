b4eba5c7518e5993f4ea3179e9fbf8d63f9e6dba
Legend-45 <62772811+Legend-45@users.noreply.github.com>
2021-04-26 09:13:45 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { act } from "react-dom/test-utils";
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
