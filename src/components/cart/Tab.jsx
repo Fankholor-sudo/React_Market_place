3d18bb8c6001c5ef7012d28ea3970f0a2cacd893
2185695 <2185695@students.wits.ac.za>
2021-05-19 18:27:34 +0200

    price incrementation works on the tab item

--- src/components/cart/Tab.jsx
+++ src/components/cart/Tab.jsx
@@ -2,18 +2,34 @@ import React,{useState} from 'react'
 import { Button, Row, Col, Card } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
 
-function Tab({ name, price, desc, itemNo, img }) {
-    const [numItem,setNumItem] = useState({value: 1})
+function Tab({ name, price, desc, itemNo, img , setDisplay}) {
+    const prices = Number(price)
+    // const [removed, setRemoved] = useState(false)
+    const [numItem,setNumItem] = useState({
+        value: 1,
+        price: prices
+    })
+
+    // const remove = (e) =>{
+    //     e.preventDefault()
+    //     setRemoved(true)
+    // }
 
     const increment = (e) =>{
-        console.log(numItem.value)
-        setNumItem({value: numItem.value + 1})
+        console.log(numItem.price)
+        setNumItem({
+            value: numItem.value + 1,
+            price: numItem.price + prices
+        })
     }
 
     const decrement = (e) =>{
-        console.log('called: ',numItem.value)
+        console.log(numItem.price)
         if(numItem.value > 1){
-            setNumItem({value: numItem.value - 1})
+            setNumItem({
+                value: numItem.value - 1,
+                price: numItem.price - prices
+            })
         }
     }
     return (
@@ -22,6 +38,7 @@ function Tab({ name, price, desc, itemNo, img }) {
                 boxShadow: '5px 0px 8px 6px rgba(0,0,0,0.08)',
                 marginRight: '2rem',
                 marginBottom: '15px', marginTop: '5px', width: '51rem'
+
             }}>
                 <div className="card-body" style={{margin: '-10px'}}>
                     <Row>
@@ -53,13 +70,13 @@ function Tab({ name, price, desc, itemNo, img }) {
                                         </Button>
                                     </Row>
                                     </div>
-                                    
                                 </Row>
                             </div>
                         </Col>
                         <Col>
-                            <h4 style={{marginLeft: '20px'}}>R{price}</h4>
+                            <h4 style={{marginLeft: '20px'}}>R{numItem.price}</h4>
                             <Button 
+                                onClick={()=>setDisplay(true)}
                                 style={{ background: '#06042E', width: '7rem', marginTop: '25px', marginLeft: '20px' }}>
                                 Remove
                             </Button>
