cd0a83eca11c01c0e39253ce9d7200d95a8eb70e
2185695 <2185695@students.wits.ac.za>
2021-05-22 17:48:49 +0200

    remove button working but not fully

--- src/components/Deshboard/itemBox.jsx
+++ src/components/Deshboard/itemBox.jsx
@@ -1,10 +1,10 @@
 import React,{useState} from 'react';
 import Modal from 'react-modal';
-import 'font-awesome/css/font-awesome.min.css';
+//import 'font-awesome/css/font-awesome.min.css';
 
-
-    function ItemBox({image, itemName, orgPrice, index, desc}){
+    function ItemBox({image, itemName, orgPrice, index, desc }){
    
+        const price = Number(orgPrice)
         const [modalIsOpen, setModalIsOpen] = useState(false);
 
         const addCartItems = () => {
@@ -15,8 +15,9 @@ import 'font-awesome/css/font-awesome.min.css';
                item = [{
                    "PICTURE": image,
                    "NAME": itemName,
-                   "PRICE": orgPrice,
+                   "PRICE": price,
                    "DESCRIPTION": desc,
+                   "COUNT": 1,
                    "KEY": index
                }]
                localStorage.setItem("CartItems", JSON.stringify(item))
@@ -25,12 +26,13 @@ import 'font-awesome/css/font-awesome.min.css';
                item = {
                    "PICTURE": image,
                    "NAME": itemName,
-                   "PRICE": orgPrice,
+                   "PRICE": price,
                    "DESCRIPTION": desc,
+                   "COUNT": 1,
                    "KEY": index
                }
                var storedItems = JSON.parse(localStorage.getItem("CartItems"));
-               storedItems.push(item);;
+               storedItems.push(item);
                localStorage.setItem("CartItems", JSON.stringify(storedItems));
            }
        }
