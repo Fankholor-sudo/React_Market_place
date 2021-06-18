80abaadeccfe266e54dbd5577e1067d67467c2e6
Legend-45 <2172765@students.wits.ac.za>
2021-06-18 13:09:56 +0200

    updated

--- src/components/Deshboard/searchResults.jsx
+++ src/components/Deshboard/searchResults.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from './Header';
 import Footer from "./Footer";
-import Nav from '../Deshboard/catNavBar';
+import Nav from './catNavBar';
 import ItemBox from "./itemBox";
 
 
